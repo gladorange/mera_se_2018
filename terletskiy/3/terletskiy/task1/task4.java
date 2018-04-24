c32ab226004a830e5995cccec949b0f811be541d
vadim terletskiy <vadim.terletskiy@gmail.com>
2018-04-24 10:34:24 +0300

    home work 4. painter and enum.

---
+++ terletskiy/3/terletskiy/task1/task4.java
@@ -0,0 +1,9 @@
+package terletskiy.task1;
+
+public class task4 {
+    public static void main(String[] args) {
+        Painter painter = new Painter("Painter1", Painter.Style.CUBISM);
+        painter.printInfo();
+
+    }
+}
