7e48110ad329ad00773a85167e8a2615bef590db
Vadim Terletskiy <Vadim.Terletskiy@gmail.ru>
2018-05-10 09:34:44 +0300

    I think there is  some error (typo error) in task description. I think all requirements were met.

---
+++ terletskiy/6/terletskiy/task1/Worker.java
@@ -0,0 +1,8 @@
+package terletskiy.task1;
+
+public class Worker extends Employee {
+
+    public Worker(String fam, String imya, String otche, Manager manager) {
+        super(fam, imya, otche, manager);
+    }
+}
