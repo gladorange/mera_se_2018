f7848b2f325ce43df9f670bc14827fa7e00bcd97
Eugene <eezudin87@gmail.com>
2018-05-16 01:45:03 +0300

    Lesson7

---
+++ zudin/1/zudin/task7/Person.java
@@ -0,0 +1,18 @@
+package zudin.task7;
+
+/**
+ * Created by eugene on 13.05.18.
+ */
+public class Person {
+
+    protected String firstName;
+    protected String lastName;
+    protected String dateOfBirth;
+
+    public Person(String firstName, String lastName, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+
+    }
+}
