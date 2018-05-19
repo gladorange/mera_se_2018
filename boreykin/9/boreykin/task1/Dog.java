981d94385457f419f71a8711df407c277aa29d49
VBoreikin <vboreyk@mera.ru>
2018-05-19 21:01:18 +0300

    Lesson 9: Animal classes

---
+++ boreykin/9/boreykin/task1/Dog.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boreykin.task1;
+
+/**
+ *
+ * @author flyer
+ */
+@AnimalDescription(val = "Собака. Бывает кусачей. Но только от жизни собачей.")
+public class Dog extends Animal
+{
+    private static final int DEFAULT_BARK_VOLUME = 5;
+    private int barkVolume;
+    
+    public Dog()
+    {
+        this("", -1);
+    }
+    
+    public Dog(String name, int barkVolume)
+    {
+        super(name);
+        this.barkVolume = (0 <= barkVolume) ? barkVolume : DEFAULT_BARK_VOLUME;
+    }
+    
+    @Override
+    public void sayHello()
+    {
+        String prefix;
+        String suffix;
+        
+        if(barkVolume > DEFAULT_BARK_VOLUME)
+        {
+            prefix = "ГАВ";
+            suffix = "лаю громко";
+        }
+        else if((barkVolume > 0)
+            && (barkVolume < DEFAULT_BARK_VOLUME))
+        {
+            prefix = "Тяф";
+            suffix = "лаю тихо";
+        }
+        else if(barkVolume == 0)
+        {
+            prefix = "Здравствуйте";
+            suffix = "лаять не могу, поэтому просто улыбаюсь";
+        }
+        else
+        {
+            // Default
+            prefix = "Гав";
+            suffix = "я лаю";
+        }
+        
+        System.out.printf("%s, я собака %s, %s.%n", prefix, this.getName(), suffix);
+    }
+    
+    @Override
+    public boolean equals(Object ob)
+    {
+        if(this == ob)
+        {
+            return true;
+        }
+        
+        return (null != ob)
+            && (ob instanceof Dog)
+            && (super.equals((Animal)ob))
+            && (((Dog)ob).barkVolume == this.barkVolume);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + this.barkVolume;
+        return hash;
+    }
+    
+    public int getBarkVolume()
+    {
+        return barkVolume;
+    }
+}
---
+++ boreykin/9/boreykin/task1/Zebra.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boreykin.task1;
+
+/**
+ *
+ * @author flyer
+ */
+@AnimalDescription(val = "Зебра. Вместе с матросом верхом на ней ходит в психическую атаку.")
+public class Zebra extends Animal
+{
+    private static final int DEFAULT_STRIPES = 10;
+    private static final int MIN_STRIPES = 2;
+    private int stripes;
+    
+    public Zebra(String name, int stripes)
+    {
+        super(name);
+        this.stripes = (MIN_STRIPES < stripes) ? stripes : DEFAULT_STRIPES;
+    }
+    
+    @Override
+    public void sayHello()
+    {
+        System.out.printf("Иго-го, я зебра %s с %d полосами.%n", this.getName(), stripes);
+    }
+    
+    @Override
+    public boolean equals(Object ob)
+    {
+        if(this == ob)
+        {
+            return true;
+        }
+        
+        return (null != ob)
+                && (ob instanceof Zebra)
+                && (super.equals((Animal)ob))
+                && (((Zebra)ob).stripes == this.stripes);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.stripes;
+        return hash;
+    }
+    
+    public int getStripeNumber()
+    {
+        return stripes;
+    }
+}
